9a8d80993331590e3eed9bb1bd25d24e50056f15
Andrew Turner <galen.turner@me.com>
2011-06-09 12:04:30 -0500

    initial site files
    Signed-off-by: Andrew Turner <galen.turner@me.com>

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
+	<link rel="apple-touch-icon" href="/images/touch-icon-iphone.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="/images/touch-icon-ipad.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="/images/touch-icon-iphone4.png" />
+	<title>Untitled</title>
+	<link rel="stylesheet" href="/css/main.css" type="text/css" media="all" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js" type="text/javascript"></script>
+	<script src="/scripts/js/functions.js" type="text/javascript"></script>
+</head>
+<body>
+
+</body>
+</html>
